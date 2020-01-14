115edc70b11200274c7e74f8dbc2c25877dbc3c7
Tikhoe <tikhoe@averly.com.na>
2020-01-14 16:07:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,42 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 var synth = window.speechSynthesis;
 
-const { ipcRenderer } = window.require("electron")
+// const { ipcRenderer } = window.require("electron")
 
 
 export default class App extends React.Component {
 
+  componentDidMount(){
+    // setTimeout(() => {
+    //   this.runcall()
+    // }, 15000);
+  }
+
   runcall(){
 
-    var arg = {voice:"Samantha", letter: "A", number: 114}
-    var message = 'Ticket number: ' + arg.letter + ': ' + arg.number + '. To counter number: 7.'
-    ipcRenderer.send('callmyname', {message});
+    var systemVoices = {
+      windows: "Microsoft Eva Mobile - English (United States)",
+      mac: "Samantha",
+      google: "Google UK English Female"
+    }
+
+    // var arg = {voice: systemVoices.windows, letter: "A", number: 114}
+    // var message = 'Ticket number: ' + arg.letter + ': ' + arg.number + '. To counter number: 7.'
+    // ipcRenderer.send('callmyname', {message});
 
     // Microsoft Eva Mobile - English (United States)
 
     
-    // synth.onvoiceschanged = function() {
-    //   synth.getVoices();
-    //   console.log('got them');
-    // }
+    synth.onvoiceschanged = function() {
+      synth.getVoices();
+      console.log('got them');
+    }
 
-    // var utterThis = "Ticket number! A. 114. To counter number! 1."
-    // var voices = speechSynthesis.getVoices()
-    // if (Object.keys(voices).length) {
-    //   console.log(voices);
-  
-    //   var voice = voices.filter((data)=>{ return (data.name==="Google UK English Female") })
-    //   // if(Object.keys(voice).length===0){
-    //   //     voice = voices.filter((data)=>{ return (data.name==="Fiona") })
-    //   // }
-    // }
-
-    // console.log(voice);
+    var utterThis = "Ticket number! A. 114. To counter number! 1."
+    var voices = speechSynthesis.getVoices()
+    console.log(voices);
+    if (Object.keys(voices).length) {  
+      var voice = voices.find((data)=>{ return (data.name===systemVoices.windows) })
+      console.log(voice);
+      if(voice==null){ voice = voices.find((data)=>{ return (data.name===systemVoices.google) }) }
+      console.log(voice);
+      if(voice==null){ voice = voices.find((data)=>{ return (data.name===systemVoices.mac) }) }
+      console.log(voice);
+    }
     
 
-    // utterThis = new SpeechSynthesisUtterance(utterThis)
-    // utterThis.voice = voice[0]
-    // synth.speak(utterThis)
+    utterThis = new SpeechSynthesisUtterance(utterThis)
+    utterThis.voice = voice
+    synth.speak(utterThis)
 
   }
 
